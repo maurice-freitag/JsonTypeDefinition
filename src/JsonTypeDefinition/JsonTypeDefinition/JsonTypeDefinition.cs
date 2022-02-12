aec85cf5886750dc890a17563106bd9ae115e490
Maurice Freitag <maurice.freitag@hotmail.de>
2022-02-12 09:40:58 +0100

    added base type definition

---
+++ src/JsonTypeDefinition/JsonTypeDefinition/JsonTypeDefinition.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections.Generic;
+
+namespace JsonTypeDefinition
+{
+    public abstract class JTDSchema
+    {
+        public string? Name { get; set; }
+    }
+
+    public enum JTDPrimitiveType
+    {
+        Boolean,
+        String,
+        TimeStamp,
+        Float32,
+        Float64,
+        Int8,
+        Uint8,
+        Int16,
+        Uint16,
+        Int32,
+        Uint32
+    }
+
+    public class JTDTypePropertyDefinition : JTDSchema
+    {
+        public JTDPrimitiveType Type { get; set; }
+    }
+
+    public class JTDEnumPropertyDefinition : JTDSchema
+    {
+        public IEnumerable<string>? Values { get; set; }
+    }
+
+    public class JTDElementsPropertyDefinition : JTDSchema
+    {
+        public IEnumerable<JTDTypePropertyDefinition>? Properties { get; set; }
+    }
+
+    public class JTDTypeDefinition
+    {
+        public IEnumerable<JTDSchema>? Properties { get; set; }
+    }
+}
